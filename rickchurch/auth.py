ea4db5d0cccd7735f1ee30fb258746bf5bb5b8a5
ItsDrike <itsdrikeofficial@gmail.com>
2021-06-01 18:08:39 +0200

    Move auth models to auth

--- rickchurch/auth.py
+++ rickchurch/auth.py
@@ -1,12 +1,61 @@
+import enum
 import secrets
-from typing import Optional
+from typing import NamedTuple, Optional
 
 import asyncpg
+import fastapi
 from fastapi.security.utils import get_authorization_scheme_param
 from jose import JWTError, jwt
 
 from rickchurch import constants
-from rickchurch.models import AuthResult, AuthState
+
+
+class AuthState(enum.Enum):
+    """Represents possible outcomes of a user attempting to authorize."""
+    NO_TOKEN = (
+        "There is no token provided, provide one in an Authorization header in the format "
+        "'Bearer {your token here}' or go to church's home and get one."
+    )
+    BAD_HEADER = "The Authorization header does not specify the Bearer scheme."
+    INVALID_TOKEN = "The token provided is not a valid token or has expired."
+    BANNED = "You are banned."
+    MODERATOR = "This token belongs to a moderator."
+    USER = "This token belongs to a regular user."
+
+    def __bool__(self) -> bool:
+        """Return whether the authorization was successful."""
+        return self in (AuthState.USER, AuthState.MODERATOR)
+
+    def raise_if_failed(self) -> None:
+        """Raise an HTTPException if a user isn't authorized."""
+        if self:
+            return
+        raise fastapi.HTTPException(status_code=403, detail=self.value)
+
+    def raise_unless_mod(self) -> None:
+        """Raise an HTTPException if a moderator isn't authorized."""
+        if self == AuthState.MODERATOR:
+            return
+        elif self == AuthState.USER:
+            raise fastapi.HTTPException(status_code=403, detail="This endpoint is limited to moderators")
+        self.raise_if_failed()
+
+
+class AuthResult(NamedTuple):
+    """The possible outcomes of authorization with user id."""
+    state: AuthState
+    user_id: Optional[int]
+
+    def __bool__(self) -> bool:
+        """Return whether the authorization was successful"""
+        return bool(self.state)
+
+    def raise_if_failed(self) -> None:
+        """Raise an HTTPException if a user isn't authorized."""
+        self.state.raise_if_failed()
+
+    def raise_unless_mod(self) -> None:
+        self.state.raise_unless_mod()
 
 
 async def authorized(authorization: Optional[str], asyncpg_conn: asyncpg.Connection) -> AuthResult:
--- rickchurch/models.py
+++ rickchurch/models.py
@@ -1,59 +1,8 @@
-import enum
 import re
-from typing import NamedTuple, Optional
 
 import pydantic
-from fastapi import HTTPException
 
 _RGB_RE = re.compile(r"[0-9a-fA-F]{6}")
-
-
-class AuthState(enum.Enum):
-    """Represents possible outcomes of a user attempting to authorize."""
-    NO_TOKEN = (
-        "There is no token provided, provide one in an Authorization header in the format "
-        "'Bearer {your token here}' or go to church's home and get one."
-    )
-    BAD_HEADER = "The Authorization header does not specify the Bearer scheme."
-    INVALID_TOKEN = "The token provided is not a valid token or has expired."
-    BANNED = "You are banned."
-    MODERATOR = "This token belongs to a moderator."
-    USER = "This token belongs to a regular user."
-
-    def __bool__(self) -> bool:
-        """Return whether the authorization was successful."""
-        return self in (AuthState.USER, AuthState.MODERATOR)
-
-    def raise_if_failed(self) -> None:
-        """Raise an HTTPException if a user isn't authorized."""
-        if self:
-            return
-        raise HTTPException(status_code=403, detail=self.value)
-
-    def raise_unless_mod(self) -> None:
-        """Raise an HTTPException if a moderator isn't authorized."""
-        if self == AuthState.MODERATOR:
-            return
-        elif self == AuthState.USER:
-            raise HTTPException(status_code=403, detail="This endpoint is limited to moderators")
-        self.raise_if_failed()
-
-
-class AuthResult(NamedTuple):
-    """The possible outcomes of authorization with user id."""
-    state: AuthState
-    user_id: Optional[int]
-
-    def __bool__(self) -> bool:
-        """Return whether the authorization was successful"""
-        return bool(self.state)
-
-    def raise_if_failed(self) -> None:
-        """Raise an HTTPException if a user isn't authorized."""
-        self.state.raise_if_failed()
-
-    def raise_unless_mod(self) -> None:
-        self.state.raise_unless_mod()
 
 
 class Task(pydantic.BaseModel):
