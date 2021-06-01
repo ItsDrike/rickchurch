3bf4e3060002d1cda4c86ff5d2466e69f12937e1
ItsDrike <itsdrikeofficial@gmail.com>
2021-06-01 18:20:30 +0200

    Define primary key for projects table

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS public.users (
 	user_id int8 NOT NULL,
-    name text NOT NULL,
 	key_salt text NOT NULL,
 	is_mod bool NOT NULL DEFAULT false,
 	is_banned bool NOT NULL DEFAULT false,
@@ -13,4 +12,5 @@ CREATE TABLE IF NOT EXISTS public.projects (
     position_y int4 NOT NULL,
     project_priority int4 NOT NULL,
     base64_image text NOT NULL,
+    CONSTRAINT projects_pk PRIMARY KEY (project_name)
 );
